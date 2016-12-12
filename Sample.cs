0e64edba20a444b502e6da79d632ba4793d171f1
5argon <pyasry@gmail.com>
2016-12-13 01:53:20 +0700

    first

---
+++ Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SargonCrypt;
+
+    class Sample
+    {
+        public static void Main(string[] args)
+        {
+            string alphabets = "abcdefghjklmnpqrstuvwyxzABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+            string key = "5argondesu";
+
+            SarCrypt sarCrypt = new SarCrypt(alphabets, key);
+            string sampleGen = sarCrypt.Generate(6);
+
+            Console.WriteLine("Sample SarText : " + sampleGen);
+            Console.WriteLine("Sample SarCrypt : " + sarCrypt.Encrypt(sampleGen));
+            while(true)
+            {
+                Console.WriteLine("Type something");
+                string input = Console.ReadLine();
+                Console.WriteLine(sarCrypt.Encrypt(input));
+            }
+        }
+    }
